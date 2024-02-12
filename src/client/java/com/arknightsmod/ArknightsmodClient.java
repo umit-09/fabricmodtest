5ff27811fb33fc6a1ac2114f85225fce8a4381f8
umittadelen <72702832+umit-09@users.noreply.github.com>
2024-02-12 11:33:54 +0100

    begin

---
+++ src/client/java/com/arknightsmod/ArknightsmodClient.java
@@ -0,0 +1,10 @@
+package com.arknightsmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArknightsmodClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
